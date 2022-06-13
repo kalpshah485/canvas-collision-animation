590a3b9cbf6b1d8f74f291d7c7159325640dd2d4
kalpshah485 <kalpshah485@gmail.com>
2022-06-13 11:08:40 +0530

    feat: canvas collision detection.

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -112,7 +112,7 @@ var mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
 };
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']; // Event Listeners
+var colors = ['#2185C5', '#7ECEFD', '#FF7F66']; // Event Listeners
 
 addEventListener('mousemove', function (event) {
   mouse.x = event.clientX;
@@ -124,42 +124,96 @@ addEventListener('resize', function () {
   init();
 }); // Objects
 
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
+var Particle = /*#__PURE__*/function () {
+  function Particle(x, y, radius, color) {
+    _classCallCheck(this, Particle);
 
     this.x = x;
     this.y = y;
+    this.velocity = {
+      x: (Math.random() - 0.5) * 5,
+      y: (Math.random() - 0.5) * 5
+    };
     this.radius = radius;
     this.color = color;
+    this.mass = 1;
+    this.opacity = 0;
   }
 
-  _createClass(Object, [{
+  _createClass(Particle, [{
     key: "draw",
     value: function draw() {
       c.beginPath();
       c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      c.save();
+      c.globalAlpha = this.opacity;
       c.fillStyle = this.color;
       c.fill();
+      c.restore();
+      c.strokeStyle = this.color;
+      c.stroke();
       c.closePath();
     }
   }, {
     key: "update",
-    value: function update() {
+    value: function update(particles) {
       this.draw();
+
+      for (var i = 0; i < particles.length; i++) {
+        if (this === particles[i]) continue;
+
+        if (Object(_utils__WEBPACK_IMPORTED_MODULE_0__["distance"])(this.x, this.y, particles[i].x, particles[i].y) - this.radius * 2 < 0) {
+          Object(_utils__WEBPACK_IMPORTED_MODULE_0__["resolveCollision"])(this, particles[i]);
+        }
+      }
+
+      if (this.x - this.radius <= 0 || this.x + this.radius >= innerWidth) {
+        this.velocity.x = -this.velocity.x;
+      }
+
+      if (this.y - this.radius <= 0 || this.y + this.radius >= innerHeight) {
+        this.velocity.y = -this.velocity.y;
+      } // mouse collision detection
+
+
+      if (Object(_utils__WEBPACK_IMPORTED_MODULE_0__["distance"])(mouse.x, mouse.y, this.x, this.y) < 120 && this.opacity < 0.2) {
+        this.opacity += 0.02;
+      } else if (this.opacity > 0) {
+        this.opacity -= 0.02;
+        this.opacity = Math.max(0, this.opacity);
+      }
+
+      this.x += this.velocity.x;
+      this.y += this.velocity.y;
     }
   }]);
 
-  return Object;
+  return Particle;
 }(); // Implementation
 
 
-var objects;
+var particles;
 
 function init() {
-  objects = [];
+  particles = [];
 
-  for (var i = 0; i < 400; i++) {// objects.push()
+  for (var i = 0; i < 100; i++) {
+    var radius = 20;
+    var x = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(radius, canvas.width - radius);
+    var y = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(radius, canvas.height - radius);
+    var color = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomColor"])(colors);
+
+    if (i !== 0) {
+      for (var j = 0; j < particles.length; j++) {
+        if (Object(_utils__WEBPACK_IMPORTED_MODULE_0__["distance"])(x, y, particles[j].x, particles[j].y) - radius * 2 <= 0) {
+          x = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(radius, canvas.width - radius);
+          y = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(radius, canvas.height - radius);
+          j = -1;
+        }
+      }
+    }
+
+    particles.push(new Particle(x, y, radius, color));
   }
 } // Animation Loop
 
@@ -167,9 +221,9 @@ function init() {
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y); // objects.forEach(object => {
-  //  object.update()
-  // })
+  particles.forEach(function (particle) {
+    particle.update(particles);
+  });
 }
 
 init();
@@ -198,10 +252,48 @@ function distance(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
 
+function rotate(velocity, angle) {
+  return {
+    x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+  };
+}
+
+function resolveCollision(particle, otherParticle) {
+  var xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+  var yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+  var xDist = otherParticle.x - particle.x;
+  var yDist = otherParticle.y - particle.y;
+
+  if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+    var angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+    var m1 = particle.mass;
+    var m2 = otherParticle.mass;
+    var u1 = rotate(particle.velocity, angle);
+    var u2 = rotate(otherParticle.velocity, angle);
+    var v1 = {
+      x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2),
+      y: u1.y
+    };
+    var v2 = {
+      x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2),
+      y: u2.y
+    };
+    var vFinal1 = rotate(v1, -angle);
+    var vFinal2 = rotate(v2, -angle);
+    particle.velocity.x = vFinal1.x;
+    particle.velocity.y = vFinal1.y;
+    otherParticle.velocity.x = vFinal2.x;
+    otherParticle.velocity.y = vFinal2.y;
+  }
+} // eslint-disable-next-line no-undef
+
+
 module.exports = {
   randomIntFromRange: randomIntFromRange,
   randomColor: randomColor,
-  distance: distance
+  distance: distance,
+  resolveCollision: resolveCollision
 };
 
 /***/ })
